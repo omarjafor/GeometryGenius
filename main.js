1f2c30e41e476623a56c455d0e2de85989389f7e
omarjafor <omarjaforchy@gmail.com>
2023-08-18 12:19:52 +0600

    js function

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ function getInputValue(inputId){
     const inputValue = parseFloat(inputField.value);
     return inputValue;
 }
+function setArea(elementId, areas){
+    const element = document.getElementById(elementId);
+    element.innerText = areas;
+}
+
+function calculateTriangleArea(){
+    const base = getInputValue('triangle-base');
+    const height = getInputValue('triangle-height')
+    const area = 0.5*base*height;
+    setArea('triangle-area', area);
+}
