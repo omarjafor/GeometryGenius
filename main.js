dc7da01e85bc676e7dd6e450fd8b6d9d6371c857
omarjafor <omarjaforchy@gmail.com>
2023-08-18 22:28:50 +0600

    remove item added

--- main.js
+++ main.js
@@ -8,32 +8,46 @@ function setArea(elementId, areas){
     const element = document.getElementById(elementId);
     element.innerText = areas;
 }
+
 function addOnCalculationEntry(name, area){
     const calculationEntry = document.getElementById('calculation-entry');
     const count = calculationEntry.childElementCount;
     const p = document.createElement('p');
-    p.innerHTML = `${count + 1}. ${name} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success m-2">Delete</button>`;
+    p.innerHTML = `${count + 1}. ${name} ${area} cm<sup>2</sup> <button id='delete-btn' onclick='removeItem(this.parentNode)' class="btn btn-sm btn-success m-2">Delete</button>`;
     calculationEntry.appendChild(p);
 }
+
+function removeItem(item){
+    item.remove();
+}
+
+// calculationEntry.addEventListener('click', function(e){
+        // console.log(e.target.parentNode.parentNode.childNode);
+        // e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+        // count --;
+    // })
+
 // Onclick Function ........................................
 function calculateTriangleArea(){
     const base = getInputValue('triangle-base');
     const height = getInputValue('triangle-height')
     const area = 0.5*base*height;
     setArea('triangle-area', area);
-    addOnCalculationEntry('Triangle', area);
+    addOnCalculationEntry('Triangle Area : ', area);
 }
 function calculateRectangleArea(){
     const width = getInputValue('rectangle-width');
     const length = getInputValue('rectangle-length');
     const area = width*length;
     setArea('rectangle-area', area);
+    addOnCalculationEntry('Rectangle Area : ', area);
 }
 function calculateParallelogramArea(){
     const base = getInputValue('parallelogram-base');
     const height = getInputValue('parallelogram-height')
     const area = base * height;
     setArea('parallelogram-area', area);
+    addOnCalculationEntry('Parallelogram Area : ', area);
 }
 function calculateEllipseArea(){
     const large = getInputValue('ellipse-large');
