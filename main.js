3ee5970e5db547c833ecccebc67bf5330975c635
omarjafor <omarjaforchy@gmail.com>
2023-08-18 18:44:22 +0600

    function js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Reusable Function ........................................
 function getInputValue(inputId){
     const inputField = document.getElementById(inputId);
     const inputValue = parseFloat(inputField.value);
@@ -7,12 +8,20 @@ function setArea(elementId, areas){
     const element = document.getElementById(elementId);
     element.innerText = areas;
 }
-
+function addOnCalculationEntry(name, area){
+    const calculationEntry = document.getElementById('calculation-entry');
+    const count = calculationEntry.childElementCount;
+    const p = document.createElement('p');
+    p.innerHTML = `${count + 1}. ${name} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success m-2">Delete</button>`;
+    calculationEntry.appendChild(p);
+}
+// Onclick Function ........................................
 function calculateTriangleArea(){
     const base = getInputValue('triangle-base');
     const height = getInputValue('triangle-height')
     const area = 0.5*base*height;
     setArea('triangle-area', area);
+    addOnCalculationEntry('Triangle', area);
 }
 function calculateRectangleArea(){
     const width = getInputValue('rectangle-width');
